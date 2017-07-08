c03604d3e981e5cb8e19f4e4466c1c5a8aaa47b2
Phumin Chesdmethee <phumin@sawasdee.org>
2017-07-08 22:46:05 +0700

    Up to version 0.48.1
    Up to version 0.48.1

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name=hass --restart=always --net=host -itd  -v /volume1/docker/homeassistant:/config homeassistant/home-assistant:0.47.1
+docker run --name=hass --restart=always --net=host -itd  -v /volume1/docker/homeassistant:/config homeassistant/home-assistant:0.48.1
