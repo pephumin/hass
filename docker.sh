2ccc7487c05b1473968bf19ee4194275e163b909
Phumin Chesdmethee <phumin@sawasdee.org>
2017-07-22 15:39:29 +0700

    Testing mysql and fail
    Testing mysql and fail

--- docker.sh
+++ docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run --name=hass --restart=always --net=host -itd  -v /volume1/docker/homeassistant:/config homeassistant/home-assistant:0.48.1
+docker run --name=hass --restart=always --net=host -itd  -v /volume1/docker/homeassistant:/config homeassistant/home-assistant:0.48.1 --link mysql:mysql
+
+#docker run --name=mysql --rm -it --net=host mysql mysql -h 127.0.0.1 -uroot -p
