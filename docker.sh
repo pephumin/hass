1512c1b52f7dfc4716dd246d0de9e5fd4c7f1da1
Phumin Chesdmethee <phumin@sawasdee.org>
2017-08-01 13:02:09 +0700

    Big upgrade
    Big upgrade

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker run --name=hass --restart=always --net=host -itd  -v /volume1/docker/homeassistant:/config homeassistant/home-assistant:0.48.1 --link mysql:mysql
+docker run --name=hass --restart=always --net=host -itd  -v /volume1/docker/homeassistant:/config homeassistant/home-assistant:0.50
+#docker run --name=hass --restart=always --net=host -itd  -v /volume1/docker/homeassistant:/config homeassistant/home-assistant:0.48.1 --link mysql:mysql
 
 #docker run --name=mysql --rm -it --net=host mysql mysql -h 127.0.0.1 -uroot -p
