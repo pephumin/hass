a469bcbfeb56109f6a4bd6d64f1e66687dfb32f6
Phumin Chesdmethee <phumin@sawasdee.org>
2017-08-07 00:20:46 +0700

    Delete mqtt_debug.sh

--- mqtt_debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# monitor receiving message
-/usr/local/mosquitto/bin/mosquitto_sub -d -t "homeassistant/#"
-
-# test sending message
-/usr/local/mosquitto/bin/mosquitto_pub -h 192.168.1.100 -p 1883 -t "homeassistant/binary_sensor/garden/config" -m '{"name": "garden", "device_class": "motion"}'
