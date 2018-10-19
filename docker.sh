5f95844cc1490767f3700d5b75eddfae01fafa6e
pephumin <phumin@sawasdee.org>
2018-10-19 12:23:37 +0700

    Re-Index
    Re-Index

--- docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -d --name=hass -v /volume1/docker/home-assistant:/config -e "TZ=Thailand/Bangkok" -p 8123:8123 homeassistant/home-assistant:0.75.3
-
-docker run --name tasmoadmin --restart=always --net=host -tid -v /volume1/docker/mqtt/config:/mqtt/config:ro -v /volume1/docker/mqtt/log:/mqtt/log -v /volume1/docker/mqtt/data/:/mqtt/data/ toke/mosquitto
